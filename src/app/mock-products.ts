44982a4e718c4b25b81bc5c9018fe911ef96da50
Marilyne Naili <marilyne.lydia@gmail.com>
2023-09-05 12:52:44 +0200

    add class product

---
+++ src/app/mock-products.ts
@@ -0,0 +1,10 @@
+[
+    {image: 'http://kozysocks.com/1435-thickbox_default/chaussettes-rayures-fines-turquoises.jpg', prixHT: '9', nom: 'Chaussettes',quantite:0},
+    {image: 'https://www.modemarine.fr/upload/site0046modemarine/rubriques/rubrique-pulls-col-v-homme.jpg', prixHT: '39', nom: 'Pull',quantite:0},
+    {image: 'https://www.modemarine.fr/upload/site0046modemarine/rubriques/rubrique-pantalon-homme.jpg', prixHT: '79', nom: 'Pantalon',quantite:0},
+    {image: 'http://crossxwod.com/c/13-tm_home_default/t-shirts-debardeurs.jpg', prixHT: '19', nom: 'T-shirt', quantite:0},
+    {image: 'https://www.parfuma.com/media/catalog/product/cache/3/image/800x800/9df78eab33525d08d6e5fb8d27136e95/c/a/caron_pour_un_homme.jpg', prixHT: '74', nom: 'Parfum',quantite:0},
+    {image: 'http://www.bastideleconfortmedical.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/0/109701-lunettes-van-gogh-homme.jpg', prixHT: '139', nom: 'Lunette',quantite:0},
+    {image: 'http://www.tendancevetements.com/upfiles/main/Entreprise-Loisir-Section-Verticale-Sac-%C3%80-Dos-Sacs-Bandouli%C3%A8re-Des-Sacs-Homme-Documents-Mallette-Marron-Chocolat-3032.jpg', prixHT: '64', nom: 'Sac',quantite:0},
+    {image: 'http://blzjeans.com/13336-44958-thickbox/chaussures-de-ville-homme-noire-fashion-blz-jeans.jpg', prixHT: '39', nom: 'Chaussure',quantite:0},
+  ];
---
+++ src/app/product.ts
@@ -0,0 +1,6 @@
+export class Product {
+    image!: string;
+    prixHT!: number;
+    nom!: number;
+    quantite!: number;
+  }
